8b3c8991f358cabdbeaf031b25474c26ebab4ad9
568cats <568cats@gmail.com>
2026-02-24 14:48:43 +0000

    Update what is your name

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -8,7 +8,7 @@ export default function QueryProcessor(query: string): string {
   }
 
   if (query.toLowerCase().includes("name")) {
-    return "Catherine";
+    return "cseluzhy";
   }
 
   if (query.toLowerCase().includes("andrewid")) {
